f04d23ad730e1fdc82867c6c1fdd7197551d4f80
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-07-04 18:53:55 +0545

    installed frontedn using react with vite and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "tailwindcss";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white min-h-screen flex items-center justify-center">
+        <h1 className="text-red-100 bg-red-500 h-20 flex items-center ">Hello Tailwind v4</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
